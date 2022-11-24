491410f0da168e34d00c03f9f7bff7d80bea15dd
HapperKing <105974453+HapperKing@users.noreply.github.com>
2022-11-24 20:29:26 +0800

    categories rewrite

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -3,17 +3,16 @@ import React from 'react'
 const Categories = () => {
   return (
     <div className=''>
-      <header className='flex justify-evenly'>
-        <div className='flex w-[1000px] h-[80px]'>
-          <div className='flex w-1/2 justify-start space-x-5'>
-            <a href='' className=''>Dream digger</a>
-            <a href='' className=''>探索</a>
-            <a href='' className=''>提案</a>
+      <header className='flex justify-center'>
+        <div className='flex items-center w-[1000px] h-[70px] space-x-4'>
+          <div className=''>
+            <a href='' className='font-semibold font-mono'>Dream Digger</a>
           </div>
-          <div className='flex w-1/2 justify-end space-x-5 mr-6'>
-            <a href='' className=''>登入</a>
-            <a href='' className=''>註冊</a>
-            <a href='' className=''>facebook登入</a>
+          <div>
+            <a href='' className='text-[14px]'>探索</a>
+          </div>
+          <div>
+            < a href='' className='text-[14px]'>提案</a>
           </div>
         </div>
       </header>

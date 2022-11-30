7cf6365f2dbf6ac9221518a3db28f6634f28c6cf
HapperKing <105974453+HapperKing@users.noreply.github.com>
2022-12-01 01:28:56 +0800

    categories UI remake

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Categories = () => {
   return (
     <div className=''>
-      <header className='flex justify-center'>
+      <header className='flex justify-center mt-[1px]'>
         <div className='flex justify-start w-[500px] h-[70px]'>
           <div className='flex items-center'>
             <div className='flex space-x-5 h-[30px]'>
@@ -35,25 +35,13 @@ const Categories = () => {
           </div>
         </div>
       </header>
-      <div className='flex justify-center'>
-        <div className='flex w-[500px] h-[100px]'>
+      <div className='flex justify-center mt-[2px]'>
+        <div className='flex justify-start h-[100px]'>
           <div className='flex justify-start'>
-            <div className='flex items-center space-x-4'>
-              <div className='w-[100px]'>
-                <p className='text-[20px]'>專案性質</p>
-              </div>
-              <p className='text-[20px]'>+</p>
-              <div className='w-[100px]'>
-                <p className='text-[20px]'>主題分類</p>
-              </div>
-            </div>
+            search
           </div>
-        </div>
-        <div className='flex justify-end w-[500px] h-[100px]'>
-          <div className='flex '>
-            <div className='flex items-center'>
-              search2 
-            </div>
+          <div className=''>
+            search
           </div>
         </div>
       </div>

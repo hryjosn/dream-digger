92fac51254c3cd2a451b1d866a6f721703ff9766
HapperKing <105974453+HapperKing@users.noreply.github.com>
2022-12-10 16:55:42 +0800

    categories UI the search part remake

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -14,7 +14,7 @@ const Categories = () => {
                 <a href='./categories' className='text-[14px]'>探索</a>
               </div>
               <div>
-                <a href=''  className='text-[14px]'>提案</a>
+                <a href='' className='text-[14px]'>提案</a>
               </div>
             </div>
           </div>
@@ -35,32 +35,65 @@ const Categories = () => {
           </div>
         </div>
       </header>
-      <div className='flex justify-center mt-[2px]'>
-        <div className='w-[500px]'>
+      <div className='flex justify-center'>
+        <div className='w-[500px] flex'>
           <div className=''>
-            <div>
-              search
-            </div>
+            專案性質
+          </div>
+          <div>
+            主題分類
           </div>
         </div>
-        <div className='w-[500px]'>
+        <div className='w-[500px] flex justify-end'>
+          <div className=''>
+            搜尋計畫
+          </div>
           <div>
-            <div className=''>
-              search
-            </div>
+            排序
           </div>
         </div>
       </div>
       <div className='flex justify-center'>
-        <div className='w-[1000px]'>
+        <div className='w-[1000px] h-[1000px]'>
           categories
         </div>  
       </div>
-      <footer className='flex justify-center'>
-        <div className='w-[1000px]'>
-          about
+      <div>
+        <footer className='flex justify-center  border-y-[1px]'>
+        <div className='w-[1000px] h-[250px]'>
+          <div className='flex'>
+            <div className='w-[250px]'>
+              <p className='text-2xl font-mono mt-[75px] font-bold'>Dream Digger</p>
+            </div>
+            <div className='w-[250px] text-[14px]'>
+              <div className='justfiy-start ml-[50px] mt-[75px] flex flex-col space-y-3'>
+                <p className='font-semibold'>幫助</p>
+                <a href='./question' className='hover:text-blue-700 text-gray-600'>常見問題</a>
+                <a href='./TermsOfUse' className='hover:text-blue-700 text-gray-600'>使用條款</a>
+                <a href='./privacy' className='hover:text-blue-800 text-gray-600'>隱私權政策</a>
+              </div>
+            </div>
+            <div className='w-[230px] text-[14px]'>
+              <div className='ustfiy-start ml-[20px] mt-[75px] flex flex-col space-y-3'>
+                <p className='font-semibold'>關於</p>
+                <a href='./about' className='hover:text-blue-800 text-gray-600'>關於我們</a>
+                <a href='./trademark' className='hover:text-blue-800 text-gray-600'>商標資源</a>
+              </div>
+            </div>
+            <div className='flex'>
+              <div>
+                <a href='./facebook'></a>
+              </div>
+            </div>
+          </div>
         </div>
-      </footer>
+        </footer>
+        <div className='flex justify-center'>
+          <div className='w-[1000px] h-[140px] flex justify-center'>
+            <p className='font-serif font-light text-[13px] text-zinc-500 mt-[40px]'>@Dream Digger 酷酷工作室</p>
+          </div>
+        </div>
+      </div>
     </div>   
   )
 }

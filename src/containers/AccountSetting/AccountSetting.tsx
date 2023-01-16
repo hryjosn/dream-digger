2ef571f7ffd710791ad2c29723647c81d3af74aa
HapperKing <105974453+HapperKing@users.noreply.github.com>
2023-01-16 14:08:21 +0800

    AccountSetting-UI

---
+++ src/containers/AccountSetting/AccountSetting.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AccountSetting = () => {
+  return (
+    <div>
+        <div>
+
+        </div>
+        <div className='flex space-x-5 mt-()'>
+          <a href='' className=''/>贊助紀錄
+          <a href='' className=''/>追蹤紀錄
+          <a href='' className=''/>提案紀錄
+          <a href='' className=''/>個人設定
+          <a href='' className=''/>團隊設定
+          <a href='' className=''/>修改密碼
+        </div>
+    </div>
+  )
+}
+
+export default AccountSetting;

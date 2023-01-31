dfa599c5b7bea0e48ca6d3e07aab8a16214c6fd7
HapperKing <105974453+HapperKing@users.noreply.github.com>
2023-01-31 16:00:40 +0800

    feature signupSlice generic

--- src/slice/SignUp/SignUpSlice.ts
+++ src/slice/SignUp/SignUpSlice.ts
@@ -13,41 +13,27 @@ const initialState: SignUpType<T> ={
     setErrorMsg: '',
 }
 
-export const SignSlice  = <
-    T,
-    Reducers extends SliceCaseReducers<SignUpType<T>>
->({
-    name = '',
+export const SignSlice  =  createSlice({
+    name: 'sign',
     initialState,
-    reducers,
-}: {
-    name: string
-    initialState: SignUpType<T>
-    reducers: ValidateSliceCaseReducers<SignUpType<T>, Reducers>
-}) => { 
-    return createSlice({
-        name: 'sign',
-        initialState,
-        reducers:{
-            getSignUpName:(state: SignUpType<T>, action: PayloadAction<T>) => {
-                state.params.name = action.payload
-            },
-            getSignUpEmail(state: SignUpType<T>, action: PayloadAction<T>) {
-                state.params.email = action.payload
-            },
-            getSignUpPassword(state: SignUpType<T>, action: PayloadAction<T>) {
-                state.params.password = action.payload
-            },
-            verifyPassword(state: SignUpType<T>, action: PayloadAction<T>) {
-                state.verifyPassword = action.payload
-            },
-            setErrorMsg(state: SignUpType<T>, action: PayloadAction<T>) {
-                state.setErrorMsg = action.payload;
-            },
-            ...reducers,
+    reducers:{
+        getSignUpName:(state, action: PayloadAction<T>) => {
+            state.params.name = action.payload
         },
-    })
-}
+        getSignUpEmail(state, action: PayloadAction<T>) {
+            state.params.email = action.payload
+        },
+        getSignUpPassword(state, action: PayloadAction<T>) {
+            state.params.password = action.payload
+        },
+        verifyPassword(state, action: PayloadAction<T>) {
+            state.verifyPassword = action.payload
+        },
+        setErrorMsg(state, action: PayloadAction<T>) {
+            state.setErrorMsg = action.payload;
+        },
+    },
+})
 
 export const { getSignUpName, getSignUpEmail, getSignUpPassword, verifyPassword, setErrorMsg} = SignSlice.actions;
-export default SignSlice.reducers
+export default SignSlice.reducer
